111e4ae20e5642a47c5199606bc6e967c358ab8e
Akimasa Takeuchi <akimasa.takeuchi@gaiax.com>
2015-02-27 19:40:27 +0900

    rebase commit

--- main.go
+++ main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"os"
-	"github.com/codegangsta/cli"
 	"yoseage/yose"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "yoseage"
-	app.Usage = "compressor file"
-	app.Version = "0.0.1"
-	app.Action = func(c *cli.Context) {
-		var dirPaths = []string{"/root/document"}
-		yose.CheckTarget(dirPaths)
-	}
-	app.Run(os.Args)
+	var dirPaths = []string{"/root/document"}
+	yose.CheckTarget(dirPaths)
 }
