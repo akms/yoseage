6c3496f84b7cede9f6aced04784f8283fb403fed
akimasa <0155take@gmail.com>
2015-02-28 14:51:38 +0900

    fix Commpressfile bug

--- yose/compression_func.go
+++ yose/compression_func.go
@@ -69,7 +69,7 @@ func CompressionFile(tw *tar.Writer, fileinfo []os.FileInfo, dirname string) {
 			tmpname := filepath.Join(dirname,file.Name())
 			//fmt.Println(filepath.Base(file.Name()))
 			fmt.Println(tmpname)
-			body, _ := ioutil.ReadFile(tmpname)
+			body, _ := ioutil.ReadFile(file.name())
 			if err = tw.WriteHeader(&tar.Header{Mode: int64(file.Mode()), Size: file.Size(), ModTime: file.ModTime(), Name: tmpname}); err != nil {
 				log.Fatal(err)
 			}
