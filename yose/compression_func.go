f33a80c8b844fe84a3d840908661c0a4f93d2d7c
Akimasa Takeuchi <akimasa.takeuchi@gaiax.com>
2015-03-08 14:49:11 +0900

    update file

--- yose/compression_func.go
+++ yose/compression_func.go
@@ -10,32 +10,40 @@ import (
 	"path/filepath"
 )
 
-func MakeFile() (gw *gzip.Writer, tw *tar.Writer, file *os.File) {
+var (
+	gw   *gzip.Writer
+	tw   *tar.Writer
+	file *os.File
+)
+
+func MakeFile() (*gzip.Writer, *tar.Writer, *os.File) {
 	var (
-		err error
+		hostname string
+		err      error
 	)
-	if file, err = os.Create("/tmp/hoge.tar.gz"); err != nil {
+	if hostname, err = os.Hostname(); err != nil {
+		log.Fatal(err)
+	}
+	hostname = "/tmp/" + hostname + ".tar.gz"
+	if file, err = os.Create(hostname); err != nil {
 		log.Fatal(err)
 	}
 	gw = gzip.NewWriter(file)
 	tw = tar.NewWriter(gw)
-	return
+	return gw, tw, file
 }
 
 func CheckTarget(dirPaths []string) {
 	var (
 		fileinfo []os.FileInfo
 		err      error
-		gw       *gzip.Writer
-		tw       *tar.Writer
-		file     *os.File
 	)
 	for _, dirpath := range dirPaths {
 		ChangeDir(dirpath)
 		if fileinfo, err = ioutil.ReadDir(dirpath); err != nil {
 			log.Fatal(err)
 		}
-		_,dirname := filepath.Split(dirpath)
+		_, dirname := filepath.Split(dirpath)
 		gw, tw, file = MakeFile()
 		CompressionFile(tw, fileinfo, dirname)
 	}
@@ -51,38 +59,53 @@ func CheckTarget(dirPaths []string) {
 
 func CompressionFile(tw *tar.Writer, fileinfo []os.FileInfo, dirname string) {
 	var (
-		err          error
-		tmp_fileinfo []os.FileInfo
+		err            error
+		tmp_fileinfo   []os.FileInfo
+		change_dirpath string
 	)
-	for _, file := range fileinfo {
-		if file.Mode()&os.ModeSymlink != os.ModeSymlink {
-			if file.IsDir() == true {
-				if tmp_fileinfo, err = ioutil.ReadDir(file.Name()); err != nil {
+	for _, infile := range fileinfo {
+
+		if infile.IsDir() == true {
+			if tmp_fileinfo, err = ioutil.ReadDir(infile.Name()); err != nil {
+				log.Fatal(err)
+			}
+			/*if err = infile.Walk(file.Name(),walkFn); err != nil {
+				log.Fatal(err)
+			}*/
+			change_dirpath, _ = filepath.Abs(infile.Name())
+			fmt.Println(change_dirpath)
+			ChangeDir(change_dirpath)
+			dirname = filepath.Join(dirname, infile.Name())
+			CompressionFile(tw, tmp_fileinfo, dirname)
+			dirname, _ = filepath.Split(dirname)
+			change_dirpath, _ = filepath.Split(change_dirpath)
+			fmt.Println(change_dirpath)
+			ChangeDir(change_dirpath)
+		} else {
+			if infile.Mode()&os.ModeSymlink != os.ModeSymlink {
+				tmpname := filepath.Join(dirname, infile.Name())
+				evalsym, _ := os.Readlink(infile.Name())
+				linkname, _ := filepath.Abs(evalsym)
+				fmt.Println(linkname)
+				body, _ := ioutil.ReadFile(infile.Name())
+				if err = tw.WriteHeader(&tar.Header{Mode: int64(infile.Mode()), Size: infile.Size(), ModTime: infile.ModTime(), Name: tmpname, Linkname: linkname}); err != nil {
 					log.Fatal(err)
 				}
-				//		err = filepath.Walk(file.Name(),walkFn)
-				change_dirpath,_ := filepath.Abs(file.Name())
-				fmt.Println(change_dirpath)
-				ChangeDir(change_dirpath)
-				dirname = filepath.Join(dirname,file.Name())
-				CompressionFile(tw, tmp_fileinfo, dirname)
-				dirname,_ = filepath.Split(dirname)
-				change_dirpath,_ = filepath.Split(change_dirpath)
-				fmt.Println(change_dirpath)
-				ChangeDir(change_dirpath)				
+				if _, err = tw.Write(body); err != nil {
+					log.Fatal(err)
+				}
 			} else {
-				tmpname := filepath.Join(dirname,file.Name())
+				tmpname := filepath.Join(dirname, infile.Name())
 				fmt.Println(tmpname)
-				body, _ := ioutil.ReadFile(file.Name())
-				if err = tw.WriteHeader(&tar.Header{Mode: int64(file.Mode()), Size: file.Size(), ModTime: file.ModTime(), Name: tmpname}); err != nil {
+				body, _ := ioutil.ReadFile(infile.Name())
+				if err = tw.WriteHeader(&tar.Header{Mode: int64(infile.Mode()), Size: infile.Size(), ModTime: infile.ModTime(), Name: tmpname}); err != nil {
 					log.Fatal(err)
 				}
 				if _, err = tw.Write(body); err != nil {
 					log.Fatal(err)
 				}
 			}
-		}else{
-			fmt.Println(file.Name())
+
 		}
 	}
 }
